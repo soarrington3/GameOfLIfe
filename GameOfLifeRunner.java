381ebbd03c991eb245570727fc5fed09bd7e238f
soarrinton3 <bitpos@ctemplar.com>
2021-02-26 11:33:15 -0800

    feb 26

--- GameOfLifeRunner.java
+++ GameOfLifeRunner.java
@@ -1,3 +1,12 @@
 public class GameOfLifeRunner {
-    
+    public static void main(String[] args){
+    int[][]input={
+        {0,1,1,0,1},
+        {0,1,1,0,1},
+        {0,1,1,0,1},
+        {0,1,1,0,1},
+    };
+    GameOfLife p1= new GameOfLife(input);
+    p1.print();
+}
 }
