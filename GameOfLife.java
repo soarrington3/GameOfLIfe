87df76d8a7f8d956c9ac20391ed36aad06fda09d
soarrinton3 <bitpos@ctemplar.com>
2021-02-25 17:15:23 -0800

    v2

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.awt.*;
 /*
 / method to draw the cell background
 / method to draw the cell
@@ -7,6 +8,7 @@ public class GameOfLife{
     private int[][] states;
 
     public GameOfLife(int[][]input){
+       // this.board();
         states=new int[input.length][input[0].length];
         for (int row=0;row<input.length;row++){
             for(int col=0;col<input[row].length;col++){
@@ -16,15 +18,19 @@ public class GameOfLife{
             
         }
     }
+    public void cell(int x,int y){
+        this.cell(x, y);
+    }
     public void nextState(){ //method to advance state of cell
         for(int row=0;row<states.length;row++){
             for(int col=0;col<states[row].length;col++){
+                
 
             }
         }
 
     }
-
+    
     public boolean getStatus(int row,int col){ // get curent state of cell
         int temp=states[row][col];
         if(temp==0){
@@ -33,8 +39,16 @@ public class GameOfLife{
 
         return true;  
     }
-    public void boardState(){
-
+    
+    public void print(){
+        for(int row=0;row<states.length;row++){
+            for(int col=0;col<states[row].length;col++){
+                System.out.printf("%2d",states[row][col]);
+                
+            }
+            System.out.println();
+        }
+       
+        
     }
-    public 
 }
